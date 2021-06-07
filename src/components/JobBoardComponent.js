a237e63022d04e58f8b34e5c2922d4593df631a3
WilliamMcColl <billymccoll1985@gmail.com>
2021-06-07 16:25:29 +0100

    updating job component again

--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -1,19 +1,53 @@
 import React from 'react';
 
-const JobBoardComponent = ({job}) => (
-    <div className="flex bg-white shadow-md m-4 p-4">
-        <div>
-            <img src={job.logo} alt={job.company} />
+const JobBoardComponent = ({job: {
+    company,
+    logo,
+    isNew,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+},}) => {
+
+    const langAndTools =[];
+
+    if (tools) {
+        langAndTools.push(...tools);
+    }
+
+    if (languages) {
+        langAndTools.push(...languages);
+    }
+
+
+    return (
+        <div className="flex bg-white shadow-md m-4 p-6">
+            <div>
+                <img src={logo} alt={company} />
+            </div>
+            <div className="flex flex-col justify-between ml-4">
+                <h3 className="font-bold text-blue-500">{company}</h3>
+                <h2 className="font-bold text-xl">{position}</h2>
+                <p className="text-gray-400">
+                    {postedAt} · {contract} · {location}
+                </p>
+            </div>
+            <div className="flex items-center ml-auto">
+                { langAndTools ? (
+                        langAndTools.map(langAndTool => (
+                            <span className="bg-blue-500 font-bold text-white m-2 p-2 rounded">{langAndTool}</span>
+                        ))
+                    ) : ( '' ) 
+                }
+            </div>
         </div>
-        <div>
-            <h3>{job.company}</h3>
-            <h2>{job.position}</h2>
-            <p>
-                {job.postedAt} - {job.contract} - {job.location}
-            </p>
-        </div>
-        <div></div>
-    </div>
-)
+    )
+        }
 
 export default JobBoardComponent;
